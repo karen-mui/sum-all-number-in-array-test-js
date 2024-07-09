981389c4275a23d60155244d6b6393370838bed7
Karen Mui <karen_mui@hotmail.com>
2024-07-08 20:32:23 -0400

    finished array recursion assignment

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  
+    for (let element of array) {    
+      if (Array.isArray(element)) {
+       sum += sumItems(element)
+      } else {
+        sum += element;
+      }
+    }
+
+  return sum;
 }
 
 module.exports = sumItems;
